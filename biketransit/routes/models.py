ff31ce5adca2fb6545bf8401ae1980ae0addf4b2
Matthew Timmons <@dukes.jmu.edu>
2024-04-30 10:32:59 -0400

    Created a route model template.

--- biketransit/routes/models.py
+++ biketransit/routes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class route(models.Model):
+    name = models.CharField(max_length=30)
+    name = models.IntegerField(max_length=20)
+    coordinates = models.CharField()
