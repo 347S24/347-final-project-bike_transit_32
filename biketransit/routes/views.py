763b3bae6a3ad1fd15c8d48f09e61c8e63f01746
CalebJames1 <james2cd@dukes.jmu.edu>
2024-05-06 15:05:28 -0400

    Saved Routes page shows routes

--- biketransit/routes/views.py
+++ biketransit/routes/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-
+from .models import Route, Location
 # Create your views here.
 
 def index(request):
     """View function for home page of site."""
-
     context = {
         'num_routes': 0,
     }
 
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', context=context)
 
 def login(request):
     """View function for home page of site."""
@@ -33,8 +32,9 @@ def routes(request):
     return render(request, 'route_finder.html', context)
 
 def saved_routes(request):
-    
+    routes = Route.objects.values()
     context = {
         'num_routes': 0,
+        'routes': routes,
     }
     return render(request, 'saved_routes.html', context)
